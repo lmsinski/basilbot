87cab43c113accbc5e781e35fee8fb31b3bbeb9d
mcguia <amcg10@gmail.com>
2020-10-26 19:39:29 -0400

    add new stamina command

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const Discord = require('discord.js');
-require('dotenv').config();
+const moment = require('moment');
 const client = new Discord.Client();
+const { prefix, token, formatter } = require('./config.json');
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('message', message => {
-	if (message.content === '!basil') {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'basil') {
 		message.channel.send('Hi!');
+	} else if (command === 'stam') {
+		const amt = parseInt(args[0]);
+		
+		if (isNaN(amt)) {
+			return message.reply('not a valid number!');
+		} else if (amt <= 1 || amt > 119) {
+			return message.reply('you must input a number between 1 and 119!');
+		}
+		const min = (120 - amt) * 8;
+		const currentDate = message.createdAt;
+		const newDate = new Date(currentDate.getTime() + min*60000);
+		return message.reply(moment(newDate).format(formatter));
 	}
 });
-console.log(process.env.DISCORD_SECRET_ID);
-client.login(process.env.DISCORD_SECRET_ID);
+
+client.login(token);
